20825b817ab3076e17c9af858dc3a6bf39627273
alfvip <salfonso@vippinn.com>
2024-06-12 20:54:34 -0300

    prueba

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': '01 Hello World MFH',
+    'version': '1.0',
+    'author': 'Marlon Falcon Hernandez',
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'depends': ['base'],
+    'data': [
+              'views/ej_pet_views.xml',
+              'security/ir.model.access.csv',
+            ],
+    'installable': True,
+    'images': ['static/description/banner.jpg'],
+}
